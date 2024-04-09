97340adbfd494b55b75c9c0823d868a8b518184f
Giorgio Ventura <gventuraagramonte@gmail.com>
2024-04-09 12:22:42 -0500

    client gateway

---
+++ src/config/envs.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config'
+import * as Joi from 'joi'
+
+interface EnvVars {
+    PORT: number;
+    PRODUCTS_MS_HOST: string;
+    PRODUCTS_MS_PORT: number
+}
+
+const envsSchema = Joi.object({
+    PORT: Joi.number().required(),
+    PRODUCTS_MS_HOST: Joi.string().required(),
+    PRODUCTS_MS_PORT: Joi.number().required(),
+})
+.unknown(true)
+
+const {error, value} = envsSchema.validate(process.env)
+
+if(error){
+    throw new Error(`Config validation error: ${error.message}`)
+}
+
+const envVars: EnvVars = value
+
+export const envs = {
+    port: envVars.PORT,
+    productsMsHost: envVars.PRODUCTS_MS_HOST,
+    productsMsPort: envVars.PRODUCTS_MS_PORT
+}
